3fffb04e3fff470e8993efe41eb8dbe423491424
leandrosardi <leandro.sardi@expandedventure.com>
2022-09-18 09:44:30 -0300

    #1

--- examples/deploy.rb
+++ examples/deploy.rb
@@ -1,4 +1,4 @@
 require_relative '../lib/pampa.rb'
 require_relative '../config.rb'
 
-BlackStack::Pampa.deploy
+BlackStack::Pampa.deploy('../config.rb', '../worker.rb')
--- worker.rb
+++ worker.rb
@@ -41,21 +41,25 @@ PARSER = BlackStack::SimpleCommandLineParser.new(
 )
 
 # creating logfile
-l = BlackStack::LocalLogger.new("worker.#{"%09d" % PARSER.value('id').to_s}.log")
+l = BlackStack::LocalLogger.new('worker.'+'%09d' % PARSER.value('id').to_s+'.log')
   
 begin
     # log the paramers
     l.log 'STARTING WORKER'
-    l.log "Parameters: #{PARSER.to_s}"
 
+    # show the parameters
+    # TODO: replace this hardocded array for method `PARSER.params`.
+    # reference: https://github.com/leandrosardi/simple_command_line_parser/issues/7
+    #['id','delay','debug','pampa','config'].each { |param| l.log param + ': ' + PARSER.value(param).to_s }
+    
     # require the pampa library
-    l.logs "Requiring pampa (debug=#{PARSER.value('debug')}, pampa=#{PARSER.value('pampa')})... "
+    l.logs 'Requiring pampa (debug='+(PARSER.value('debug') ? 'true' : 'false')+', pampa='+PARSER.value('pampa')+')... '
     require 'pampa' if !PARSER.value('debug')
     require PARSER.value('pampa') if PARSER.value('debug')
     l.done
 
     # requiore the config.rb file where the jobs are defined.
-    l.logs "Requiring config (config=#{PARSER.value('config')})"
+    l.logs 'Requiring config (config='+PARSER.value('config')+')'
     require PARSER.value('config')
     l.done
 
@@ -67,44 +71,44 @@ begin
 
     # start the loop
     while true
+        # get the start loop time
+        l.logs 'Starting loop... '
+        start = Time.now()
+        l.done
+
         begin
-            # get the start loop time
-            l.logs 'Starting loop... '
-            start = Time.now()
-            l.done
-            
             # TODO: get the next task to process
             # TODO: process the tasks
-
-            # get the end loop time
-            l.logs 'Ending loop... '
-            finish = Time.now()
-            l.done
-            
-            # get different in seconds between start and finish
-            # if diff > 30 seconds
-            l.logs 'Calculating loop duration... '
-            diff = finish - start
-            l.logf "done (#{diff.to_s})"
-
-            if diff < PARSER.value('delay')
-                # sleep for 30 seconds
-                n = PARSER.value('delay')-diff
-                
-                l.logs "Sleeping for #{n} seconds... "
-                sleep n
-                l.done
-            else
-                l.log "No sleeping. The loop took #{diff} seconds."
-            end
         rescue SignalException, SystemExit, Interrupt => e
             # note: this catches the CTRL+C signal.
             # note: this catches the `kill` command, ONLY if it has not the `-9` option.
             raise e
         rescue => e
-            l.logf "Error: #{e.message}.\n\nBacktrace: #{e.backtrace.join("\n")}"
+            l.logf 'Error: '+e.to_console
         rescue 
             l.logf 'Unknown Error.'
+        end
+
+        # get the end loop time
+        l.logs 'Ending loop... '
+        finish = Time.now()
+        l.done
+            
+        # get different in seconds between start and finish
+        # if diff > 30 seconds
+        l.logs 'Calculating loop duration... '
+        diff = finish - start
+        l.logf 'done ('+diff.to_s+')'
+
+        if diff < PARSER.value('delay')
+            # sleep for 30 seconds
+            n = PARSER.value('delay')-diff
+                
+            l.logs 'Sleeping for '+n.to_label+' seconds... '
+            sleep n
+            l.done
+        else
+            l.log 'No sleeping. The loop took '+diff.to_label+' seconds.'
         end
     end # while true
 rescue SignalException, SystemExit, Interrupt
@@ -112,7 +116,7 @@ rescue SignalException, SystemExit, Interrupt
     # note: this catches the `kill` command, ONLY if it has not the `-9` option.
     l.logf 'Process Interrumpted.'
 rescue => e
-    l.logf "Fatal Error: #{e.message}.\n\nBacktrace: #{e.backtrace.join("\n")}"
+    l.logf 'Fatal Error: '+e.to_console
 rescue 
     l.logf 'Unknown Fatal Error.'
 end
