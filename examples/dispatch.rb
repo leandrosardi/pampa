6ea7f3baa87c9aa9fa4ba843f09890b5e45c0163
leandrosardi <leandro.sardi@expandedventure.com>
2022-09-18 18:04:07 -0300

    #1

--- examples/dispatch.rb
+++ examples/dispatch.rb
@@ -1,15 +1,23 @@
 require_relative '../lib/pampa.rb'
 require_relative '../config.rb'
 
+l = BlackStack::Pampa.logger
+
 while true
     # assign workers to each job
+    l.logs 'Assigning workers to jobs... '
     BlackStack::Pampa.stretch
+    l.done
+
+    # relaunch expired tasks
+    l.logs 'Relaunching expired tasks... '
+    BlackStack::Pampa.relaunch
+    l.done
 
     # dispatch tasks to each worker
-    #BlackStack::Pampa.restart
-
-    # dispatch tasks to each worker
+    l.logs 'Dispatching tasks to workers... '
     BlackStack::Pampa.dispatch
+    l.done
 
     # sleep
     sleep(5)
